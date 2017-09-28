06ec1e410758b6a2a25f9047c63483678372127a
Vitalii Prots <vitalikprocj@gmail.com>
2017-09-28 15:06:03 +0300

    First-commit

---
+++ app.js
@@ -0,0 +1,50 @@
+var printMultipleTimes = function(howManyTimes, whatToDraw) {
+	for(var i = 0; i < howManyTimes; i++) {
+		console.log(i + ":" + whatToDraw);
+	}
+}
+printMultipleTimes(6, " =^.^=");
+
+var fifthLetter = function(name) {
+	if(name.length < 5) {
+		return;
+	}
+	return console.log("dfnskjgnsg " + name[4]);
+}
+fifthLetter("Cerfduytu");
+
+var medalForScore = function (score) {
+	if(score < 3) {
+		return console.log("Бронза");
+	}
+	if(score < 7) {
+		return console.log("Срібна");
+	}
+	return console.log("Золото");
+}
+
+
+// Повна запись функції:
+var a = function () {
+	console.log("Hello World!");
+}
+a();
+
+// Коротка запись функції:
+function b() {
+	console.log("Hello world 2!!")
+}
+b();
+
+// Возврат значения из функции:
+
+
+// Homework:
+
+//	#1. Математические расчеты и функции
+
+
+
+
+
+
